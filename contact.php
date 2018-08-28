a482bf50e9ee35f6907a7cfce6c2b2f6aba51097
Prabhakar <acharyaP03@gamil.com>
2018-08-28 10:42:52 +1000

    Udated header section on contact page

--- contact.php
+++ contact.php
@@ -1,4 +1,17 @@
 <?php include 'header.php';?>
+    <header>
+      <a href="index.php" id="logo">
+        <h1>Prabhakar Acharya</h1>
+        <h2>Designer</h2>
+      </a>
+      <nav>
+        <ul>
+          <li><a href="index.php" >Portfolio</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="contact.php" class="selected">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
  <div id="wrapper">
       <section id="primary">
         <h3>General Information</h3>
